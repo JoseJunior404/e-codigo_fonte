6c6144ce153863d172ed141fc6b52256c9c96002
jose junior <jose.junior@prodepa.pa.org.br>
2025-05-27 16:29:28 -0300

    Criação do arquivo AdminSeeder para inserir um usuário admin.

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+use function Laravel\Prompts\password;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name'=>'Admin',
+            'email'=>'admin@e-comercial.com',
+            'password'=>Hash::make('12345678'),
+            'is_admin'=> 1,
+        ]);
+    }
+}
